2ae4ec554831f3033277e5173bc11f4eff3a9761
robin.wang_ext <robin.wang_ext@chinascope.com>
2021-02-25 16:49:58 +0800

    DHCP2：Hander拦截转换和Druid多数据源加载

--- src/main/java/com/robin/test/demo/config/DataSourceConfig.java
+++ src/main/java/com/robin/test/demo/config/DataSourceConfig.java
@@ -1,12 +1,10 @@
 package com.robin.test.demo.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 
@@ -14,52 +12,21 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
     /**
-     * 创建 test 数据源的配置对象
-     */
-    @Primary
-    @Bean(name = "testDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.test")
-    public DataSourceProperties testDataSourceProperties(){
-        return new DataSourceProperties();
-    }
-
-    /**
      * 创建 test 数据源
      */
+    @Primary
     @Bean(name = "testDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.test.hikari")
-    public DataSource testDataSource(){
-        DataSourceProperties properties =  this.testDataSourceProperties();
-        return createHikariDataSource(properties);
+    @ConfigurationProperties(prefix = "spring.datasource.test") // 读取 spring.datasource.orders 配置到 HikariDataSource 对象
+    public DataSource testDataSource() {
+        return DruidDataSourceBuilder.create().build();
     }
 
     /**
-     * 创建 user 数据源的配置对象
-     */
-    @Bean(name = "userDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.user")
-    public DataSourceProperties userDataSourceProperties(){
-        return new DataSourceProperties();
-    }
-
-    /**
-     * 创建 user 数据源
+     * 创建 users 数据源
      */
     @Bean(name = "userDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.user.hikari")
-    public DataSource userDataSource(){
-        DataSourceProperties properties =  this.userDataSourceProperties();
-        return createHikariDataSource(properties);
+    @ConfigurationProperties(prefix = "spring.datasource.user")
+    public DataSource userDataSource() {
+        return DruidDataSourceBuilder.create().build();
     }
-
-    private DataSource createHikariDataSource(DataSourceProperties properties) {
-        // 创建 HikariDataSource 对象
-        HikariDataSource dataSource = properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-        // 设置线程池名
-        if (StringUtils.hasText(properties.getName())) {
-            dataSource.setPoolName(properties.getName());
-        }
-        return dataSource;
-    }
-
 }
